7701df652b54f81d10dba24ff86651eef443b9f9
japrine <jprine01@gmail.com>
2019-01-07 03:59:33 -0600

    Fixed

--- filepdf.py
+++ filepdf.py
@@ -16,29 +16,36 @@ def get_filename():
 
 
 def get_page(filename, page_num):
-    with open(filename, 'rb') as f:
-        pdf_reader = PyPDF2.PdfFileReader(f)
-        qty = pdf_reader.numPages
-        page_obj = pdf_reader.getPage(page_num)
-        the_page = page_obj.extractText()
-        return the_page, qty
+    try:
+        with open(filename, 'rb') as f:
+            pdf_reader = PyPDF2.PdfFileReader(f)
+            qty = pdf_reader.numPages
+            page_obj = pdf_reader.getPage(page_num)
+            the_page = page_obj.extractText()
+            return the_page, qty
+    except FileNotFoundError:
+        print('Filename specified was not found')
+        return None, 0
 
 
 def name_from_pdf(filename):
     page_num = 0
     the_page, page_qty = get_page(filename, page_num)
-
-    while page_num <= page_qty:
-        if the_page.find('8879') == 0:
-            nameEnd = the_page.find("Spouse's name")
-            nameStart = the_page.find("Taxpayer's name") + 15
-            name = the_page[nameStart:nameEnd]
-            break
-        else:
-            print('Page is not 8879, advancing')
-            page_num += 1
-            the_page, page_qty = get_page(filename, page_num)
-    return name
+    name = None
+    if the_page:
+        while page_num <= page_qty:
+            if the_page.find('8879') == 0:
+                nameEnd = the_page.find("Spouse's name")
+                nameStart = the_page.find("Taxpayer's name") + 15
+                name = the_page[nameStart:nameEnd]
+                break
+            else:
+                print('Page is not 8879, advancing')
+                page_num += 1
+                the_page, page_qty = get_page(filename, page_num)
+        return name
+    else:
+        return name
 
 
 def name_parse(name):
@@ -100,10 +107,13 @@ def make_dir(name):
 def main():
     # filename = get_filename()
     if filename:
-        name = name_from_pdf()
+        name = name_from_pdf(filename)
         if name:
             name = name_parse(name)
             name = dis_dialog(name)
             if not name:
-                break
+                return
             make_dir(name)
+
+
+main()
