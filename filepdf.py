703bded1598adf1aeecc81357d7e639ccebdbe4d
japrine <jprine01@gmail.com>
2019-01-07 03:51:19 -0600

    Get args, fix cancel button.

--- filepdf.py
+++ filepdf.py
@@ -9,6 +9,12 @@ filename = '2018_12_19_14_04_32.pdf'
 base_storage_folder = '/Scans'
 
 
+def get_filename():
+    if sys.argv[1:]:
+        filename = sys.argv[1]
+        return filename
+
+
 def get_page(filename, page_num):
     with open(filename, 'rb') as f:
         pdf_reader = PyPDF2.PdfFileReader(f)
@@ -35,49 +41,69 @@ def name_from_pdf(filename):
     return name
 
 
-print('Done, Found name:', name)
-name_parse = HumanName(name)
-name = name_parse.last + ' ' + name_parse.first
-msg = 'Check that name found is correct:'
-title = 'Enter Name'
-fieldName = ['Name:']
-fieldValue = [name]
-fieldValue = easygui.multenterbox(msg, title, fieldName, fieldValue)
+def name_parse(name):
+    print('Done, Found name:', name)
+    name_parse = HumanName(name)
+    name = name_parse.last + ' ' + name_parse.first
+    return name
 
-while 1:
+
+def dis_dialog(name):
+    msg = 'Check that name found is correct:'
+    title = 'Enter Name'
+    fieldName = ['Name:']
+    fieldValue = [name]
+    fieldValue = easygui.multenterbox(msg, title, fieldName, fieldValue)
+
+    while 1:
+        if fieldValue is None:
+            break
+        errmsg = ""
+        if fieldValue[0] == "":
+            errmsg = 'Name is required'
+        if errmsg == "":
+            break
+        fieldValue = easygui.multenterbox(errmsg, title, fieldName, fieldValue)
+
     if fieldValue is None:
-        break
-    errmsg = ""
-    if fieldValue[0] == "":
-        errmsg = 'Name is required'
-    if errmsg == "":
-        break
-    fieldValue = easygui.multenterbox(errmsg, title, fieldName, fieldValue)
-
-if fieldValue is None:
-    print('Move Canceled')
-else:
-    name = fieldValue[0]
-    print('Name has been verified as:', name)
+        name = None
+        print('Move Canceled')
+    else:
+        name = fieldValue[0]
+        print('Name has been verified as:', name)
+    return name
 
 
-def move_file(file_name):
+def move_file(name, filename):
     try:
-        rename(file_name, '/Scans/{}/{}'.format(name, file_name))
+        rename(filename, '/Scans/{}/{}'.format(name, filename))
     except FileExistsError:
         print('File name already exists, cannot move.')
     else:
         print('File has been moved.')
 
 
-if path.isdir('/Scans/{}'.format(name)) is True:
-    print('Folder already exists, moving file.')
-    move_file(file_name)
-else:
-    try:
-        mkdir('/Scans/{}'.format(name))
-    except OSError:
-        print('Failed creating directory')
+def make_dir(name):
+    if path.isdir('/Scans/{}'.format(name)) is True:
+        print('Folder already exists, moving file.')
+        move_file(name, filename)
     else:
-        print('Created directory for', name)
-        move_file(file_name)
+        try:
+            mkdir('/Scans/{}'.format(name))
+        except OSError:
+            print('Failed creating directory')
+        else:
+            print('Created directory for', name)
+            move_file(name, filename)
+
+
+def main():
+    # filename = get_filename()
+    if filename:
+        name = name_from_pdf()
+        if name:
+            name = name_parse(name)
+            name = dis_dialog(name)
+            if not name:
+                break
+            make_dir(name)
